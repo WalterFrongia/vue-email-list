709b2357d76d99c6df517a842b5b2fdce3777b06
WalterFrongia <walter.frongia@libero.it>
2021-09-28 18:38:32 +0200

    Installazione axios e implementazione axios then per stampare email e ciclo for per iterare le email e applicazione del v-for per stampare array con email

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,37 @@
 <template>
   <div>
-  
+    <ul>
+      <li v-for="email in emailRandomica" :key="email">{{emailRandomica}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import axios  from 'axios';
+import axios from 'axios';
 
 export default {
-
   components: {
    
   },
-  mounted() {
+  data: function(){
+        return{
+          emailRandomica:[],
+        }
+    },
+   mounted() {
     console.log(axios);
+
+    for(let i = 0; i < 10; i++){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((resp) => {
+          console.log(resp.data.response);
+          this.emailRandomica.push (resp.data.response);
+        });
+      }
   }
 }
 </script>
 
 <style lang="scss">
-
 
 </style>
