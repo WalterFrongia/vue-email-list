a1f19968ddd32b7d9bc32c0e0f51433ff58755fe
WalterFrongia <walter.frongia@libero.it>
2021-09-28 16:31:37 +0200

    Installato axios e importato in components app

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <template>
-  <div id="app">
+  <div>
   
   </div>
 </template>
 
 <script>
-
+import axios  from 'axios';
 
 export default {
-  name: 'App',
+
   components: {
    
+  },
+  mounted() {
+    console.log(axios);
   }
 }
 </script>
 
 <style lang="scss">
-#app {
 
-}
+
 </style>
